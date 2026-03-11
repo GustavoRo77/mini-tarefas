1bf4bf4bec58932c6a70034390fa75fbbc344dc5
Gustavo Maioli Rossetti Pereira <gustavo.pereira16@alunos.sc.senac.br>
2026-03-11 13:41:21 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mini-tarefas',
+  webDir: 'dist'
+};
+
+export default config;
